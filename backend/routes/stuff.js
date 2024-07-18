2796d7527ce5c69e541978d2eb5d48813bb99d6b
Maxime Porta <maxime.portapro@gmail.com>
2024-07-18 16:07:19 +0200

    authentification

---
+++ backend/routes/stuff.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const stuffCtrl = require("../controller/stuff");
+const auth = require('../middleware/auth');
+
+router.post("/", auth, stuffCtrl.createThing);
+router.put("/:id", auth, stuffCtrl.modifyThing);
+router.delete("/:id", auth, stuffCtrl.deleteThing);
+router.get("/:id", auth, stuffCtrl.getOneThing);
+router.get("/", auth, stuffCtrl.getAllThing);
+
+module.exports = router;
